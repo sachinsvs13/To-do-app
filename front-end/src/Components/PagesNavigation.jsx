163aefbd7f805ac2186cccbbad16c1870c977fb0
Sachin V S <sachinsvs13@gmail.com>
2026-01-10 22:00:56 +0530

    code optimised

--- front-end/src/Components/PagesNavigation.jsx
+++ front-end/src/Components/PagesNavigation.jsx
@@ -9,16 +9,38 @@ import { Link } from "react-router-dom";
 
 export default function PagesNavigation() {
   const [IsActive, setIsActive] = useState(true);
+  const [activeItem, setActiveItem] = useState(1);
 
   const handleComponent = () => {
     setIsActive((prev) => !prev);
   };
+
+  const pages = [
+    { name: "My Day", icon: <IoSunnyOutline className="icon" />, path: "/" ,id : 1},
+    { name: "Important", icon: <FaRegStar className="icon" />, path: "/important",id : 2 },
+    { name: "Planned", icon: <CiCalendarDate className="icon" />, path: "/planned",id : 3 },
+    { name: "Task", icon: <BiTask className="icon" />, path: "/tasks" ,id : 4}
+  ]
+  console.log(activeItem)
   return (
     <div className={`${IsActive ? "side-component" : "none"}`}>
       {IsActive ? (
         <div>
           <FaGripLines className="top-icon" onClick={handleComponent} />
-          <div className="list">
+          {
+            
+            pages.map((page) => (
+              <Link
+                key={page.name}
+                to={page.path}
+                onClick={() => setActiveItem(page.id)}
+                className={`items ${activeItem === page.id ? 'active' : ''}`}
+              >
+                {page.icon}
+                {page.name}
+              </Link>
+            ))}
+{/* 
             <Link to="/" className="items">
               <IoSunnyOutline className="icon" />
               My Day
@@ -34,8 +56,8 @@ export default function PagesNavigation() {
             <Link to="/tasks" className="items">
               <BiTask className="icon" />
               Task
-            </Link>
-          </div>
+            </Link> */}
+
         </div>
       ) : (
         <FaGripLines className="top-icon" onClick={handleComponent} />
